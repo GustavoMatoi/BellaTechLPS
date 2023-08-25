2b980a32ebaf4c3d3b501b8b8e48489137007507
Gustavo Vaz Teixeira <87538302+GustavoMatoi@users.noreply.github.com>
2023-08-25 17:24:31 -0300

    Telas da BellaTech

---
+++ src/main/java/bella/tech/bellatech/BellaTech.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bella.tech.bellatech;
+
+import view.JFrameMain;
+
+/**
+ *
+ * @author gutei
+ */
+public class BellaTech {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        JFrameMain jf = new JFrameMain();
+        jf.setVisible(true);
+
+    }
+}
