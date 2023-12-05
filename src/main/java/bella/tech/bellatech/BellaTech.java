0d78066b4ed3118f2de1461ac5971670f2780c55
Gustavo Vaz Teixeira <87538302+GustavoMatoi@users.noreply.github.com>
2023-12-05 17:58:43 -0300

    feat: Documento de especificação de requisitos

--- src/main/java/bella/tech/bellatech/BellaTech.java
+++ src/main/java/bella/tech/bellatech/BellaTech.java
@@ -27,18 +27,6 @@ public class BellaTech {
 
     
     public static void main(String[] args) throws IOException{
-        try {
-            Stripe.apiKey = "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";
-            CustomerListParams params = CustomerListParams.builder().build();
-            CustomerCollection customers = Customer.list(params);
-            System.out.println(customers);
-            
-        } catch (Exception e){
-            System.out.println(e);
-
-        }
-        
-        //System.out.println("Hello World!");
         JFrameMain jf = new JFrameMain();
         jf.setVisible(true);
         
